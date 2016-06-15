e639571927296c9a3e612a3414897dec9224cccb
Alex <alexander.salamanca@gmail.com>
2016-06-14 23:43:01 -0500

    removed unnec assignments and commented out prints

--- exe/B.py
+++ exe/B.py
@@ -13,7 +13,7 @@ def reduce_when_disjoint(x, y):
 	return [a,b+d-c]
 
 #def calculate_new_non_overlapping_intervals(x, y):
-def reduce(x, y):
+def reduce_overlapping_intervals(x, y):
 	a = x[0]
 	b = x[1]
 	
@@ -32,8 +32,6 @@ def reduce(x, y):
 	if (a > c):
 		if (a < d):
 			a_ = c
-			b_ = a
-			c_ = a
 			if (b > d):
 				d_ = b
 			else:
@@ -43,8 +41,6 @@ def reduce(x, y):
 	if (b > c):
 		if (a < c):
 			a_ = a
-			b_ = c
-			c_ = c
 			if (b > d):
 				d_ = b
 			else:
@@ -52,8 +48,6 @@ def reduce(x, y):
 			return [a_, d_]
 		else:
 			a_ = c
-			b_ = a
-			c_ = a
 			if (b > d):
 				d_ = b
 			else:
@@ -65,8 +59,6 @@ def reduce(x, y):
 			a_ = a
 		else:
 			a_ = c
-		b_ = c
-		c_ = c 
 		if (b > d):
 			d_ = b
 		else:
@@ -76,8 +68,6 @@ def reduce(x, y):
 		
 	if (c > a):
 		a_ = a
-		b_ = c
-		c_ = c 
 		if (b > d):
 			d_ = b
 		else:
@@ -87,8 +77,6 @@ def reduce(x, y):
 	if (d > a):
 		if (c > a):
 			a_ = a
-			b_ = c
-			c_ = c
 			if (b > d):
 				d_ = b
 			else:
@@ -96,8 +84,6 @@ def reduce(x, y):
 			return [a_, d_]
 		else:
 			a_ = c
-			b_ = a
-			c_ = a
 			if (b > d):
 				d_ = b
 			else:
@@ -107,35 +93,25 @@ def reduce(x, y):
 	if (d > b):
 		if (a < c):
 			a_ = a
-			b_ = c
-			c_ = c
 			d_ = d
 			return [a_, d_]
 		
 	if (a == c):
 		a_ = a
 		if (b > d):
-			b_ = d
-			c_ = d
 			d_ = b
 			return [a_, d_]
 		else:
-			b_ = b
-			c_ = b
 			d_ = d
 			return [a_, d_]
 	
 	if (b == d):
 		if (a < c):
 			a_ = a
-			b_ = c
-			c_ = c
 			d_ = d
 			return [a_, d_]
 		else:
 			a_ = c
-			b_ = a
-			c_ = a
 			d_ = d
 			return [a_, d_]
 	
@@ -163,9 +139,9 @@ def answer(intervals):
 
 	n = len(interv)
 	
-	print()
-	print("interv", interv)
-	print("n", n)
+	#print()
+	#print("interv", interv)
+	#print("n", n)
 	
 	# transform all t in interv into non overlapping tuples:
 	i = 0
@@ -178,7 +154,7 @@ def answer(intervals):
 		interv.remove(u)
 		
 		if (overlap(t, u)):
-			interv.append(reduce(t, u))
+			interv.append(reduce_overlapping_intervals(t, u))
 		else:
 			interv.append(reduce_when_disjoint(t, u))
 			
@@ -189,20 +165,14 @@ def answer(intervals):
 		interv = interv2
 		n = len(interv)
 		
-		print()
-		print("interv", interv)
-		print("n", n)
+		#print()
+		#print("interv", interv)
+		#print("n", n)
 
+	return interv[0][1]-interv[0][0]
 		
-	t_intervals = 0
-
-	#for i,j in s.values():
-	#	t_intervals = t_intervals + (j-i)
-	#
-	return t_intervals
-		
-#t = [[1, 2]]
-t = [[1, 3], [3, 6]]
+t = [[1, 2]]
+#t = [[1, 3], [3, 6]]
 #t = [[1, 3], [6, 8]]
 #t = [[10, 14], [4, 18], [19, 20], [19, 20], [13, 20]]	
 
