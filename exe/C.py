4ff2c7aea498a90e50ee763d879d720db441013d
Alex <alexander.salamanca@gmail.com>
2016-06-15 22:47:03 -0500

    solved the problem correctly; i didnt took into account mixup between x and y, and i forgot to check if the current cell was infected

--- exe/C.py
+++ exe/C.py
@@ -1,72 +1,79 @@
 
+def ppp(p):
+	for i in p:
+		#s = ""
+		#for j in i:
+		#	s = s + " " + str(j)
+		print(i)
 
-def get_neighbors(population, i, j, rows, columns, strength):
-
-	if ( i-1 >= 0 ):
-		if (population[i-1][j] != -1 and population[i-1][j] <= strength):
-			yield [i-1, j]
 		
-	if ( j-1 >= 0 ):
-		if (population[i][j-1] != -1 and population[i][j-1] <= strength):
-			yield [i, j-1]
-	
-	if ( j+1 < columns ):
-		if (population[i][j+1] != -1 and population[i][j+1] <= strength):
-			yield [i, j+1]
-	
-	if ( i+1 < rows ):
-		if (population[i+1][j] != -1 and population[i+1][j] <= strength):
-			yield [i+1, j]
-		
-	
-def infect_and_hunt(population, y, x, rows, columns, strength):
+def infect_and_hunt(p, x, y, rows, cols, s):
 
-	neighbors = get_neighbors(population, y, x, rows, columns, strength)
-	
-	for n in neighbors:
-		i = n[0]
-		j = n[1]
-		if ( 0 <= population[i][j]):
-			if ( population[i][j] <= strength ):
-				population[i][j] = -1
-				infect_and_hunt(population, i, j, rows, columns, strength)
+	is_left_infectable  = x-1 >= 0 and x-1 < cols and y >= 0 and y < rows and p[y][x] == -1 and p[y][x-1] != -1 and p[y][x-1] <= s 
+	is_right_infectable = x+1 < cols and x+1 >= 0 and y >= 0 and y < rows and p[y][x] == -1 and p[y][x+1] != -1 and p[y][x+1] <= s 
+	is_upper_infectable = y-1 >= 0 and y-1 < rows and x >= 0 and x < cols and p[y][x] == -1 and p[y-1][x] != -1 and p[y-1][x] <= s 
+	is_lower_infectable = y+1 < rows and y+1 >= 0 and x >= 0 and x < cols and p[y][x] == -1 and p[y+1][x] != -1 and p[y+1][x] <= s 
 
+	#print()
+	#print()
+	#ppp(p)
+
+	if (is_left_infectable):
+		p[y][x-1] = -1
+		infect_and_hunt(p, x-1, y, rows, cols, strength)
+
+	if (is_right_infectable):
+		p[y][x+1] = -1
+		infect_and_hunt(p, x+1, y, rows, cols, strength)
+
+	if (is_upper_infectable):
+		p[y-1][x] = -1
+		infect_and_hunt(p, x, y-1, rows, cols, strength)
+
+	if (is_lower_infectable):
+		p[y+1][x] = -1
+		infect_and_hunt(p, x, y+1, rows, cols, strength)
 	
 def answer(population, x, y, strength):
 	
 	rows = len(population)
-	columns = len(population[0])
+	cols = len(population[0])
 
-	if ( 0 <= population[y][x] and population[y][x] <= strength ):
+	r = population[y][x]
+	if ( 0 <= r and r <= strength ):
 		population[y][x] = -1	
 
-	infect_and_hunt(population, y, x, rows, columns, strength)
+	infect_and_hunt(population, x, y, rows, cols, strength)
 
 	return population
 
 	
 
 #population = [[1, 2, 3], [2, 3, 4], [3, 2, 1]]
-#y = 0
+#population = [[1, 2, 3], [2, 3, 4]]
 #x = 0
+#y = 0
 #strength = 2
 
 ## [[-1, -1, 3], [-1, 3, 4], [3, 2, 1]]
 
-population = [[6, 7, 2, 7, 6], [6, 3, 1, 4, 7], [0, 2, 4, 1, 10], [8, 1, 1, 4, 9], [8, 7, 4, 9, 9]] 
-x = 2 
-y = 1 
-strength = 5 
+#population = [[6, 7, 2, 7, 6], [6, 3, 1, 4, 7], [0, 2, 4, 1, 10], [8, 1, 1, 4, 9], [8, 7, 4, 9, 9]] 
+#x = 2 
+#y = 1 
+#strength = 5 
 
 #population = [[9, 3, 4, 5, 4], [1, 6, 5, 4, 3], [2, 3, 7, 3, 2], [3, 4, 5, 8, 1], [4, 5, 4, 3, 9]]
-#y = 2
-#x = 1
-#strength = 5
+population = [[7, 7, 7, 7, 7], [7, 7, 3, 7, 7], [2, 3, 3, 3, 7], [7, 7, 3, 7, 7], [7, 7, 7, 7, 7]]
+x = 0
+y = 2
+strength = 5
 
 #population = [[9, 3, 4, 5, 4], [1, 6, 5, 4, 3], [2, 3, 7, 3, 2]]
 #y = 2
 #x = 1
 #strength = 5
-
-print(answer(population, x, y, strength))
+ppp(population)
+print()
+print()
+ppp(answer(population, x, y, strength))
 				
