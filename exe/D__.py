1d197e3bb4d4899c01a0fa80beb7d6c755a0d7ea
Alex <alexander.salamanca@gmail.com>
2016-06-18 15:02:53 -0500

    added missing code

--- exe/D__.py
+++ exe/D__.py
@@ -32,17 +32,17 @@ def is_idx_outside_all_tuples_in_range(idx, r):
 	is_not_inside_tuple_j = True
 	j = 0
 	while is_inside_range and is_not_inside_tuple_j:
-		is_not_inside_tuple_j = (r[j][0]>idx or idx>r[j][1])
+		is_not_inside_tuple_j = not(r[j][0]<idx<r[j][1])
 		j = j+1
-
+		
 	return is_not_inside_tuple_j
 	
-	3,true
-	5,false
-	12,
-	
-	4,8
-	10,15
+	#3,true
+	#5,false
+	#12,
+	#
+	#4,8
+	#10,15
 	
 def generate_subs(c, positions):
 	# this function will generate all the possible substrings in c
@@ -64,6 +64,19 @@ def answer(chunk, word):
 	m = len(word)
 	position = get_positions_for_sub(chunk, word)
 	affected = get_pairs_of_affected_subs(positions,n,m)
+	shortest_len = 21
+	
+	candidates = list()
+	
+	for i in generate_subs(chunk, affected):
+		length_i = len(i)
+		if length_i < shortest_len:
+			shortest_len = len(i)
+			candidates.pop(0)
+			candidates.insert(0, i)
+		else:
+			if length_i == shortest_len and i not in candidates:
+				candidates.append(i)
 	
 	print 'n', n, 'm', m
 	print 'positions'
@@ -73,6 +86,8 @@ def answer(chunk, word):
 	# if the intervals overlap, that is one case
 	# because i need to deactivate the replacement on the overlap
 
+	candidates.sort()
+	return candidates[0]
 
 
 c = 'lolol'
