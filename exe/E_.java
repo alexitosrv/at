6e00915e85859ba5d349c2c17205bf74e8e3aa3c
Alex <alexander.salamanca@gmail.com>
2016-06-22 23:44:11 -0500

    apparently it solves its pouring when it rains

--- exe/E_.java
+++ exe/E_.java
@@ -3,51 +3,33 @@
 public class E_ {   
 
     public static int answer(int[] heights) { 
-
         int n = heights.length;
-		int z = 0;
 		int j;
-		int k;
+		int z = 0;
 		int left = -Integer.MIN_VALUE;
 		int right = -Integer.MIN_VALUE;
-		int i = 0;
 		int i_lef = 0;
 		int i_rig = 0;
 
-		while(i<n){
-			System.out.println("start with i= "+i);
-			
+		int i = 0;
+		while(i < n){
+		
 			if (left < heights[i]){ 
-				System.out.println("left compared with current height is shorter");
 				left = heights[i];
 				i_lef = i;
 			}else{
-				System.out.println("i = "+i+", left: "+left+" >= heights[i]="+heights[i]);
-				
-				j=i;
-				System.out.println("  j = i= "+j);
+				j = i;
 				while(j < n && left > heights[j]){
-					System.out.println("     j<n and left>heights[j], j= "+j);
 					j++;
 				}
 				j--;
 
 				j = (j+1<n?j+1:n-1);
-				System.out.println("     j= "+j);
-				System.out.println("     h[j]= "+heights[j]);
 
-				k=j;
-				System.out.println("   we found a bound to the right");
-				System.out.println("   i_lef= "+i_lef);
-				System.out.println("   j= "+j);
-				System.out.println("   k= "+k);
-				
 				right = heights[j];
 				i_rig = j;
 				
 				while(j > i_lef){
-					System.out.println("        "+j+" = j>i ; go back again to find the lowest high");
-					
 					if (right < heights[j]){
 						right = heights[j];
 						i_rig = j;
@@ -55,32 +37,24 @@ public class E_ {
 					
 					j--;
 				}
-				System.out.println("     right = "+right);
-				System.out.println("     i_rig= "+i_rig);
-				
-				int m = Math.min(left, right);
-				System.out.println("     m = "+m);
-				System.out.println("   j= "+j);
-				
 				j++;
 				
-				while(j<i_rig){
-					System.out.println("        j<n && m>"+heights[j]);
-					System.out.println("        j= "+j);
+				int m = Math.min(left, right);
+				
+				while(j < i_rig){
 					System.out.println("        delta= "+Integer.toString(m-heights[j]));
 					System.out.println("        z= "+z);
 					z = z + m - heights[j];
-					System.out.println("        new z= "+z);
+					System.out.println("\n        new z= "+z);
 					j ++;
 				}
 				
 				right = -Integer.MIN_VALUE;
 				left = -Integer.MIN_VALUE;
-				i=i_rig;
-				System.out.println("  end with i= "+i);
+				i = i_rig;
 				continue;
-
 			}
+			
 			i++;
 		}
 		return z;
@@ -90,7 +64,7 @@ public class E_ {
 		//                   0  1  2  3  4  5  6
 		//                            v
 		int[] h = new int[] {1, 4, 2, 5, 1, 2, 3};
-		//int[] h = new int[] {3,2,3};
+		//int[] h = new int[] {1,4,2,2,3,5};
 		//int[] h = new int[] {1,4,2,5,1,2,3};
 		//int[] h = new int[] {1, 2, 3, 2, 1};
 //		int[] h = new int[] {1,2,3,4,3,2,1,5};
@@ -98,7 +72,7 @@ public class E_ {
 		//int[] h = new int[] {2, 5, 1, 3, 1, 2, 1, 7, 7, 6};
 		//int[] h = new int[] {2, 7, 2, 7, 4, 7, 1, 7, 3, 7};
 		//int[] h = new int[] {6, 7, 7, 4, 3, 2, 1, 5, 2}; // *
-		//int[] h = new int[] {2, 5, 1, 2, 3, 4, 7, 7, 6, 2, 7, 1, 2, 3, 4, 5, 5, 4}; //*
+		//int[] h = new int[] {2, 5, 1, 2, 3, 4, 7, 7, 6, 2, 7, 1, 2, 3, 4, 5, 5, 4}; 
 		System.out.println(answer(h));
 	}
 }
